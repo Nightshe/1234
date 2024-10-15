7b7a7547039525c334d216865fed77b1ffd93c04
Anna Perlovska <tigrik1705@gmail.com>
2024-10-16 00:16:42 +0300

    Реалізовано математичний калькулятор з класами для додавання, віднімання та множення

---
+++ MathCalculator/MathCalculator/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace MathCalculator
+{
+    public class Addition
+    {
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+    }
+
+    public class Subtraction
+    {
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+    }
+
+    public class Multiplication
+    {
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Addition addition = new Addition();
+            Subtraction subtraction = new Subtraction();
+            Multiplication multiplication = new Multiplication();
+
+            double num1 = 10;
+            double num2 = 5;
+
+            Console.WriteLine($"Addition: {num1} + {num2} = {addition.Add(num1, num2)}");
+            Console.WriteLine($"Subtraction: {num1} - {num2} = {subtraction.Subtract(num1, num2)}");
+            Console.WriteLine($"Multiplication: {num1} * {num2} = {multiplication.Multiply(num1, num2)}");
+
+            Console.ReadLine();
+        }
+    }
+}
